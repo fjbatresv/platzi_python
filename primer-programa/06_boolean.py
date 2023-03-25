4c347dd6083eb3f34a60a09c2c84a48a56989400
Javier Batres <fjbatresv@gmail.com>
2023-03-25 15:50:49 -0600

    Clase de booleans

---
+++ primer-programa/06_boolean.py
@@ -0,0 +1,12 @@
+single = True
+print(type(single))
+single = False
+print(single)
+
+# El operador not invierte al boolean
+print(not single)
+print(not True)
+
+# Inversion de un boolean si no sabemos su valor
+single = not single
+print(single)
