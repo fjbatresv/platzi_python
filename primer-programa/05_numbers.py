3e414fc7d0c4b949ca1d775e6c8171d27009de24
Javier Batres <fjbatresv@gmail.com>
2023-03-25 15:39:20 -0600

    Clase de numeros

--- primer-programa/05_numbers.py
+++ primer-programa/05_numbers.py
@@ -3,3 +3,30 @@ print(type(lives)) #int
 
 temperature = 12.12 # Python asigna el tipo de variable de forma dinamica
 print(type(temperature)) # float
+
+lives = 2
+print(lives)
+
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+# Es lo mismo que el ejemplo anterior
+lives -= 1
+print(lives)
+
+# Tambien se puede para sumar
+lives += 1
+print(lives)
+
+# En estos casos vemos que Python imprime en notación cientifica
+numberA = 4500000000000000000.1
+print(numberA)
+
+numberB = 0.0000000000000001
+print(numberB)
