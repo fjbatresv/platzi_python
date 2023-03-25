820904da79aec20fdc97ed614b96ce8423056567
Javier Batres <fjbatresv@gmail.com>
2023-03-25 17:35:27 -0600

    Operadores aritmeticos

---
+++ primer-programa/08_arith.py
@@ -0,0 +1,14 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 3)
+print(10 % 3)
+print(10 // 3) # Valor entero de la division
+print(2 ** 3) # Exponencial
+print(2 ** 3 + 3 - 7 / 1 // 4) # PEMDAS = Parentesis, exponentes, multiplicacion, division, adision, sustraccion
+'''
+Python lanza error al dividir dentro de 0
+x / 0
+'''
+print('Hola' + 'Mundo')
+print('Hola' * 3)
