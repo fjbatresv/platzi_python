917806e9bf09f07c7ccf3329498aea94ee955021
Javier Batres <fjbatresv@gmail.com>
2023-03-25 17:43:09 -0600

    Clase comparators

---
+++ primer-programa/09_comparators.py
@@ -0,0 +1,20 @@
+# > : mayor que
+print(7 > 3)
+print(3 > 7)
+# < : menor que
+print(7 < 3)
+print(3 < 7)
+# == : igual a
+print(7 == 7)
+print(7 == 5)
+# >= : mayor o igual
+print(7 >= 8)
+print(7 >= 7)
+# <= : menor o igual
+print(7 <= 7)
+print(7 <= 8)
+'''
+!= : Diferente de
+'''
+print(6 != 3)
+print(6 != 6)
