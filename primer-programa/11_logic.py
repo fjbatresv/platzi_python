0ed0c066baa66d45863680f07d8f112ab8b6629f
Javier Batres <fjbatresv@gmail.com>
2023-03-25 18:03:38 -0600

    Clase logic operators

---
+++ primer-programa/11_logic.py
@@ -0,0 +1,21 @@
+# and
+print('AND')
+print('True and True => ', True and True)
+print('True and False =>', True and False)
+print('False and False', False and False)
+print('10 > 5 and 5 < 10 => ', 10 > 5 and 5 < 10)
+print('10 > 5 and 10 < 5 => ', 10 > 5 and 10 < 5)
+
+stock = input('Ingresa el numero de stock')
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000)
+
+print('\n')
+print('*' * 100)
+print('\n')
+# or
+print('OR')
+print('True or True', True or True)
+print('False or True', False or True)
+print('False or False', False or False)
