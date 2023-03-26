28f69cc1fa48f8ff69f8c02d54a7256d5056feb1
Javier Batres <fjbatresv@gmail.com>
2023-03-25 18:05:43 -0600

    Clase not

---
+++ primer-programa/12_not.py
@@ -0,0 +1,8 @@
+print(not True)
+print(not False)
+print('NOT AND')
+print('not True and True => ', not (True and True))
+print('not True and False =>', not (True and False))
+print('not False and False', not (False and False))
+print('not 10 > 5 and 5 < 10 => ', not (10 > 5 and 5 < 10))
+print('not 10 > 5 and 10 < 5 => ', not (10 > 5 and 10 < 5))
